a1a6cafb5f9385df968a0c0c8392bebf5aec8dda
unknown <trung241az@gmail.com>
2025-03-22 00:00:17 +0700

    update UML and building file

--- ProDuelist/src/main/java/Controller/SceneManager.java
+++ ProDuelist/src/main/java/Controller/SceneManager.java
@@ -1,70 +1,70 @@
-    package Controller;
+package Controller;
 
-    import View.AccountSettingView;
-    import View.DeckBuilderView;
-    import javafx.fxml.FXMLLoader;
-    import javafx.scene.Parent;
-    import javafx.scene.Scene;
+import View.AccountSettingView;
+import View.DeckBuilderView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
-    import java.io.IOException;
+import java.io.IOException;
 
-    public class SceneManager {
-        private static SceneManager instance;
-        private DeckBuilderView deckBuilderController;
-        private final MusicController musicController = new MusicController();
+public class SceneManager {
+    private static SceneManager instance;
+    private DeckBuilderView deckBuilderController;
+    private final MusicController musicController = new MusicController();
 
-        private SceneManager() {}
+    private SceneManager() {}
 
-        public static SceneManager getInstance() {
-            if (instance == null) {
-                instance = new SceneManager();
-            }
-            return instance;
+    public static SceneManager getInstance() {
+        if (instance == null) {
+            instance = new SceneManager();
         }
-
-        public Scene loginScene() throws IOException {
-            Parent loginView = FXMLLoader.load(getClass().getResource("/FXML/LoginView/LoginView.fxml"));
-            return new Scene(loginView);
-        }
-
-        public Scene registScene() throws IOException {
-            Parent registerView = FXMLLoader.load(getClass().getResource("/FXML/RegisterView/RegisterView.fxml"));
-            return new Scene(registerView);
-        }
-
-        public Scene mainMenuScene() throws IOException {
-            Parent mainMenuScene = FXMLLoader.load(getClass().getResource("/FXML/MainMenuView/MainMenuView.fxml"));
-            return new Scene(mainMenuScene);
-        }
-
-        public Scene deckBuilderView() throws IOException {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/DeckBuilderView/DeckBuilderView.fxml"));
-            Parent deckBuilderRoot = loader.load();
-
-            deckBuilderController = loader.getController();
-
-            return new Scene(deckBuilderRoot);
-        }
-
-        public DeckBuilderView getDeckBuilderView() {
-            return deckBuilderController;
-        }
-
-        public Scene SharingDeckView() throws IOException {
-            Parent mainMenuScene = FXMLLoader.load(getClass().getResource("/FXML/SharingView/SharingView.fxml"));
-            return new Scene(mainMenuScene);
-        }
-
-        public Scene AccountSettingView() throws IOException {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/AccountSettingView/AccountSettingView.fxml"));
-            Parent accSettingScene = loader.load();
-
-            AccountSettingView controller = loader.getController();
-            controller.setMusicController(musicController);
-            return new Scene(accSettingScene);
-        }
-
-        public MusicController getMusicController() {
-            return musicController;
-        }
+        return instance;
     }
+
+    public Scene loginScene() throws IOException {
+        Parent loginView = FXMLLoader.load(getClass().getResource("/FXML/LoginView/LoginView.fxml"));
+        return new Scene(loginView);
+    }
+
+    public Scene registScene() throws IOException {
+        Parent registerView = FXMLLoader.load(getClass().getResource("/FXML/RegisterView/RegisterView.fxml"));
+        return new Scene(registerView);
+    }
+
+    public Scene mainMenuScene() throws IOException {
+        Parent mainMenuScene = FXMLLoader.load(getClass().getResource("/FXML/MainMenuView/MainMenuView.fxml"));
+        return new Scene(mainMenuScene);
+    }
+
+    public Scene deckBuilderView() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/DeckBuilderView/DeckBuilderView.fxml"));
+        Parent deckBuilderRoot = loader.load();
+
+        deckBuilderController = loader.getController();
+
+        return new Scene(deckBuilderRoot);
+    }
+
+    public DeckBuilderView getDeckBuilderView() {
+        return deckBuilderController;
+    }
+
+    public Scene SharingDeckView() throws IOException {
+        Parent mainMenuScene = FXMLLoader.load(getClass().getResource("/FXML/SharingView/SharingView.fxml"));
+        return new Scene(mainMenuScene);
+    }
+
+    public Scene AccountSettingView() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/AccountSettingView/AccountSettingView.fxml"));
+        Parent accSettingScene = loader.load();
+
+        AccountSettingView controller = loader.getController();
+        controller.setMusicController(musicController);
+        return new Scene(accSettingScene);
+    }
+
+    public MusicController getMusicController() {
+        return musicController;
+    }
+}
--- ProDuelist/src/main/java/Model/DatabaseManager.java
+++ ProDuelist/src/main/java/Model/DatabaseManager.java
@@ -1,12 +1,15 @@
 package Model;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseManager {
-    private static final String URL = "jdbc:sqlite:src/main/resources/Database/produelist.db";
+    private static final String URL = "jdbc:sqlite:" + getDatabasePath();
 
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(URL);
@@ -33,5 +36,30 @@ public class DatabaseManager {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private static String getDatabasePath() {
+        String dbName = "produelist.db";
+        String jarDir = System.getProperty("user.dir"); // Lấy thư mục chạy JAR
+        String dbPath = jarDir + "/" + dbName;
+
+        File dbFile = new File(dbPath);
+
+        // Nếu file chưa tồn tại (chạy lần đầu khi đóng gói JAR), sao chép từ resources
+        if (!dbFile.exists()) {
+            try (InputStream is = DatabaseManager.class.getResourceAsStream("/Database/" + dbName);
+                 FileOutputStream os = new FileOutputStream(dbFile)) {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, bytesRead);
+                }
+                System.out.println("Copied database to: " + dbPath);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return dbPath;
     }
 }
